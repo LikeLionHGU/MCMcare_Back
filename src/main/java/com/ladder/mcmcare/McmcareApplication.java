0231258b8ab296ec468c7e770b44d5f45b69b367
gwang <ahngwangeun@gmail.com>
2026-08-11 23:47:30 +0900

    gwang: init

---
+++ src/main/java/com/ladder/mcmcare/McmcareApplication.java
@@ -0,0 +1,13 @@
+package com.ladder.mcmcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McmcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McmcareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ladder/mcmcare/McmcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladder.mcmcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McmcareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
